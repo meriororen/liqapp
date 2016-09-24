4841a343dbfff23e695b166595ee9413eb7e9674
meriororen <meriororen@gmail.com>
2016-09-24 17:54:58 +0900

    Added to github

---
+++ Liqapp-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Liqapp-Bridging-Header.h
+//  liqapp
+//
+//  Created by Isa Ansharullah on 9/21/16.
+//  Copyright © 2016 DuldulStudio. All rights reserved.
+//
+
+#ifndef Liqapp_Bridging_Header_h
+#define Liqapp_Bridging_Header_h
+
+
+#import <LUKeychainAccess/LUKeychainAccess.h>
+
+#endif /* Liqapp_Bridging_Header_h */
